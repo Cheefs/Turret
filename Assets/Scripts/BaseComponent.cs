7361ee9193fbaaa94a8374995e74887d5a4d927f
Likus <volk_24regin@mail.ru>
2018-08-26 19:26:32 +0700

    0.0
    Базовый класс

---
+++ Assets/Scripts/BaseComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Turret
+{
+    /// <summary>
+    /// От этого класса наслдеуем все остальные
+    /// По мере необходимости дописываем сюда переменные,
+    /// которые нужно закэшировать
+    /// </summary>
+    public class BaseComponent : MonoBehaviour
+    {
+        private GameObject _gameObject;
+        private Transform _transform;
+
+        private void Awake()
+        {
+            _gameObject = gameObject;
+            _transform = transform;
+        }
+    }
+}
